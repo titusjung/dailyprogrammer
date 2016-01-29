20073b202cf90a46f0a9c365311cfb92585266af
Titus Jung <titusjung@gmail.com>
2016-01-29 13:26:10 -0800

    vertical and horizontal reading working; going to skip print formatting; move onto solving challenge

--- nonograms/nonograms.py
+++ nonograms/nonograms.py
@@ -2,11 +2,13 @@
 def read(line):
 	l =[]
 	count =0; 
-	print (line)
-	prevChar = " "
+	prevChar=" "
+	#print (line)
 	for i,x in enumerate(line):
 		#print("current char is "+x+"  prev char is "+prevChar)
-		if i == 1 and x =="*":
+		#print("x is "+x)
+		#print(i)
+		if i == 0 and x =="*":
 			count = 1 
 		if prevChar == " " and x=="*":
 			count = 1
@@ -16,26 +18,48 @@ def read(line):
 			#print("found space")
 			l.append(count)
 			count =0
-		elif prevChar =="*" and (x=="\n") :
+		elif prevChar =="*" and "\n" in x :
+			#print("new line reached")
 			l.append(count)
-		if x=="\n":
+		if i is len(line)-1 and x is"*":
+			l.append(count)
+			#print("new line reached")
 			break
 		prevChar =x
+	#print("count is ",count) 
 	return l; 
 
 def main():
-	f = open("input.txt","r")
+	f = open("input2.txt","r")
 	noLines = sum(1 for l in f)
 	flist= []
 	print("Number of lines ",noLines)
+	matrix = []
+	tempList = []
 	f.seek(0)
-	for i in range(0,noLines):
-		flist.append(f.readline())
-	#print(flist)
-	for i in flist:
-		l = read(i)
-		print(l)
-	#l = read(flist[2])
+	for i,l in enumerate(range(0,noLines)):
+		l = f.readline()
+		tempList = []
+		for j in l:
+			if j is not "\n":
+				tempList.append(j)
+		matrix.append(tempList)
+	#print(matrix)
+	height = len(matrix)
+	hList =[]
+	vList = []
+	for i in matrix:
+		#print(i)
+		hList.append(read(i))
+	print(hList)
+	for j in range(height):
+		tempVList =[]
+		for i in range(len(matrix)):
+			tempVList.append(matrix[i][j])
+		#print(tempVList)
+		vList.append(read(tempVList))
+	print(vList)
+	#l = read(flist[4])
 	#print(l)
 
 
